9156edd7847e6b9e14cb3fb0e37ac60aa8e06c9e
Cliff Liu <cliu@uplift.com>
2019-07-09 19:07:25 -0700

    1. Two Sum

---
+++ TwoSum.java
@@ -0,0 +1,34 @@
+/* Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1]. */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]) && i != map.get(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+        }
+        return new int[0];
+    }
+}
+
+/*
+Runtime Analysis:
+Time complexity : O(n)
+We traverse the list containing n elements exactly twice. Since the hash table reduces the look up time to O(1), the time complexity is O(n).
+
+Space complexity : O(n)
+The extra space required depends on the number of items stored in the hash table, which stores exactly n elements. 
+*/
