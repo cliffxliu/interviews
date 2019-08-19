6415ffbbaf2dc46538563de542a040cea9894a49
Cliff Liu <cliu@uplift.com>
2019-08-19 09:39:59 -0700

    825. Friends Of Appropriate Ages (Friend Requests)

---
+++ FriendsOfAppropriateAges.java
@@ -0,0 +1,66 @@
+/*
+825. Friends Of Appropriate Ages (Medium)
+Some people will make friend requests. The list of their ages is given and ages[i] is the age of the ith person. 
+
+Person A will NOT friend request person B (B != A) if any of the following conditions are true:
+
+age[B] <= 0.5 * age[A] + 7
+age[B] > age[A]
+age[B] > 100 && age[A] < 100
+Otherwise, A will friend request B.
+
+Note that if A requests B, B does not necessarily request A.  Also, people will not friend request themselves.
+
+How many total friend requests are made?
+
+Example 1:
+Input: [16,16]
+Output: 2
+Explanation: 2 people friend request each other.
+
+Example 2:
+Input: [16,17,18]
+Output: 2
+Explanation: Friend requests are made 17 -> 16, 18 -> 17.
+
+Example 3:
+Input: [20,30,100,110,120]
+Output: 
+Explanation: Friend requests are made 110 -> 100, 120 -> 110, 120 -> 100.
+*/
+
+class Solution {
+    public int numFriendRequests(int[] ages) {
+        //Map age to number of occurrences
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int age: ages) {
+            map.put(age, map.getOrDefault(age, 0) + 1);
+        }
+        int res = 0;
+        //For each age a and each age b != a, if request(a, b), we will make count[a] * count[b] requests.
+        //For each age a, if request(a, a), we will make count[a] * (count[a] - 1) requests.
+        for (int a: map.keySet()) {
+            for (int b: map.keySet()) {
+                if (request(a, b)) {
+                    if (a == b) {
+                        res += (map.get(a) - 1) * (map.get(b));
+                    } else {
+                        res += map.get(a) * map.get(b);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    
+    public boolean request(int a, int b) {
+        return !(b <= 0.5 * a + 7 || b > a || (b > 100 && a < 100)); 
+        //will not make a friend request if any of these conditions are true
+    }
+}
+
+/*
+Complexity:
+Time: O(N^2)
+Space: O(1)
+*/
