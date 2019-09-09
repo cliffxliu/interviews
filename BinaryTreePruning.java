109955df46523e7898a07c0fe4cf903336b9cc6d
Cliff Liu <cliffliu@berkeley.edu>
2019-09-08 19:39:39 -0700

    814. Binary Tree Pruning

---
+++ BinaryTreePruning.java
@@ -0,0 +1,46 @@
+/*
+814. Binary Tree Pruning (Medium)
+We are given the head node root of a binary tree, where additionally every node's value is either a 0 or a 1.
+
+Return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+
+(Recall that the subtree of a node X is X, plus every node that is a descendant of X.)
+
+Example 1:
+Input: [1,null,0,0,1]
+Output: [1,null,0,null,1]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        } 
+        
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        
+        /* Check no more subtrees, root.val is 0 */
+        if (root.left == null && root.right == null && root.val == 0)  {
+            return null;
+            //everything below is pruned
+        }
+        
+        return root;
+    }
+}
+
+/*
+Complexity:
+Time: O(N). Process each node once.
+Space: O(Height of Tree). Implicit call stack in recursion.
+*/
