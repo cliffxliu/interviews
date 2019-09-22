60c2ac0d9555425fe720356fa32d22680466dd41
Cliff Liu <cliffliu@berkeley.edu>
2019-09-22 02:31:07 -0700

    695. Max Area of Island

---
+++ MaxAreaOfIsland.java
@@ -0,0 +1,56 @@
+/*
+695. Max Area of Island (Medium)
+Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+
+Example 1:
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
+
+Example 2:
+[[0,0,0,0,0,0,0,0]]
+Given the above grid, return 0.
+Note: The length of each dimension in the given grid does not exceed 50.
+*/
+
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, dfsArea(grid, i, j));
+                }
+            }
+        }
+        return maxArea;
+    }
+    
+    public int dfsArea(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+        int currArea = 1;
+        int newArea = dfsArea(grid, i + 1, j) + dfsArea(grid, i - 1, j) + dfsArea(grid, i , j + 1) + dfsArea(grid, i, j - 1);
+        return currArea + newArea;
+    }
+}
+
+/*
+Complexity:
+Time: O(numRows * numColumns). We visit every cell in the matrix once.
+Space: O(numRows * numColumns). The space used to store visited squares.
+*/
