f8b21b606a1f2d4824f05de27fa3b77a7bfa3c3c
Cliff Liu <cliffliu@berkeley.edu>
2019-11-07 14:36:56 -0800

    937. Reorder Data in Log Files

---
+++ ReorderDataInLogFiles.java
@@ -0,0 +1,58 @@
+/*
+937. Reorder Data in Log Files (Easy)
+You have an array of logs.  Each log is a space delimited string of words.
+
+For each log, the first word in each log is an alphanumeric identifier.  Then, either:
+
+Each word after the identifier will consist only of lowercase letters, or;
+Each word after the identifier will consist only of digits.
+We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at least one word after its identifier.
+
+Reorder the logs so that all of the letter-logs come before any digit-log.  
+The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  
+The digit-logs should be put in their original order.
+
+Return the final order of the logs.
+
+Example 1:
+
+Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+ 
+Constraints:
+0 <= logs.length <= 100
+3 <= logs[i].length <= 100
+logs[i] is guaranteed to have an identifier, and a word after the identifier.
+*/
+
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        Arrays.sort(logs, (s1, s2) -> {
+            String[] split1 = s1.split(" ", 2); //limit = 2: pattern will be applied at most n - 1 times
+            String[] split2 = s2.split(" ", 2);
+
+            boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+
+            if(!isDigit1 && !isDigit2) {
+                // both letter-logs. 
+                int comp = split1[1].compareTo(split2[1]);
+                if (comp == 0) {
+                    return split1[0].compareTo(split2[0]);
+                } else {
+                    return comp;
+                }
+            } else if (isDigit1 && isDigit2) {
+                // both digit-logs. So keep them in original order. Java will sort correctly.
+                return 0; 
+            } else if (isDigit1 && !isDigit2) {
+                // first is digit, second is letter. bring letter to forward.
+                return 1;
+            } else {
+                //first is letter, second is digit. keep them in this order.
+                return -1; 
+            }
+        });
+        return logs;
+    }
+}
